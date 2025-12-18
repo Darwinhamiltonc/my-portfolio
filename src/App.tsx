3f50cf39ddf065566eab1a0efeb62ae638eee0bb
Darwin Hamilton <darwinhamiltoncapellan@gmail.com>
2025-12-17 20:44:46 -0400

    style: format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css'
-import NavBar from "./components/NavBar.tsx"
+import "./App.css";
+import NavBar from "./components/NavBar.tsx";
 function App() {
-
   return (
     <>
-      <header className='p-header'><NavBar /></header>
+      <header className="p-header">
+        <NavBar />
+      </header>
       <main></main>
       <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
