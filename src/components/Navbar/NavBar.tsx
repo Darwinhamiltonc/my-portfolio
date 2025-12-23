9090e63f4f487b970ea4d92cf8e18b30c5ca0072
Darwin Hamilton <darwinhamiltoncapellan@gmail.com>
2025-12-22 21:45:41 -0400

    Fix: navbar layout and language toggle alignment

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -16,43 +16,41 @@ function NavBar() {
         onClick={toggleMenu}
         aria-label="Toggle menu"
       >
-        {
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <line
-              x1="3"
-              y1="6"
-              x2="21"
-              y2="6"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-            <line
-              x1="3"
-              y1="12"
-              x2="21"
-              y2="12"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-            <line
-              x1="3"
-              y1="18"
-              x2="21"
-              y2="18"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
-        }
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line
+            x1="3"
+            y1="6"
+            x2="21"
+            y2="6"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+          <line
+            x1="3"
+            y1="12"
+            x2="21"
+            y2="12"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+          <line
+            x1="3"
+            y1="18"
+            x2="21"
+            y2="18"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </svg>
       </button>
 
       {isOpen && (
@@ -72,6 +70,7 @@ function NavBar() {
                 <path d="M8 11l4 4 4-4" />
                 <path d="M4 21h16" />
               </svg>
+              CV
             </a>
           </li>
           <li>
@@ -90,6 +89,7 @@ function NavBar() {
                 <path d="M5 8h14" />
                 <path d="M5 16h14" />
               </svg>
+              <span className="text">EN / ES</span>
             </button>
           </li>
           <li>
@@ -104,6 +104,7 @@ function NavBar() {
 
                 <path d="M4 20c0-4 4-6 8-6s8 2 8 6" />
               </svg>
+              Contacto
             </a>
           </li>
         </ul>
