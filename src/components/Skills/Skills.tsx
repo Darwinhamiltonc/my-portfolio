40ee0869ec0445a30645568c6f6c356c8cd934c1
Darwin Hamilton <darwinhamiltoncapellan@gmail.com>
2026-01-27 18:57:52 -0400

    style: add some styles to  skill section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,13 +1,25 @@
+import "./Skills.css";
+import { SiHtml5, SiCss3, SiReact, SiTypescript, SiGit } from "react-icons/si";
 function Skills() {
   return (
-    <section id="skills" className="skills-section">
-      <h2>My Skills</h2>
-      <ul className="skills-list">
-        <li>JavaScript</li>
-        <li>TypeScript</li>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>CSS</li>
+    <section className="skills-section">
+      <h2 className="skills-title">Skills</h2>
+      <ul className="skills-card">
+        <li>
+          HTML5 <SiHtml5 />
+        </li>
+        <li>
+          CSS <SiCss3 />
+        </li>
+        <li>
+          TypeScript <SiTypescript />
+        </li>
+        <li>
+          React <SiReact />
+        </li>
+        <li>
+          Git <SiGit />
+        </li>
       </ul>
     </section>
   );
